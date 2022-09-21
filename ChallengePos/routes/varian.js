1999cda7a78d761e619fb214c6aea3262f1d233d
Fernanda <mangorifernanda@gmail.com>
2022-09-22 00:17:43 +0700

    progress

--- ChallengePos/routes/varian.js
+++ ChallengePos/routes/varian.js
@@ -95,13 +95,14 @@ INNER JOIN barang bar ON bar.id_barang = var.id_barang WHERE var.id_barang = $1`
         var.varian_name,
         bar.id_barang,
         bar.nama_barang,
-          var.stock,
+          var.stok,
           var.buy_price,
           var.sell_price,
-          var.pictures
+          var.picture
 FROM varian var
 INNER JOIN barang bar ON bar.id_barang = var.id_barang WHERE barcode = $1;`
       const { rows } = await pool.query(sql, [id])
+
       if (json == 'true') {
         res.status(200).json(rows)
       } else {
@@ -115,9 +116,9 @@ INNER JOIN barang bar ON bar.id_barang = var.id_barang WHERE barcode = $1;`
   });
 
   router.post('/', async function (req, res) {
-    // console.log(req.body, req.files)
+
     const { json } = req.headers
-    // console.log(req.headers)
+
 
     try {
       let picture;
@@ -128,11 +129,10 @@ INNER JOIN barang bar ON bar.id_barang = var.id_barang WHERE barcode = $1;`
       }
       // The name of the input field (i.e. "pictures") is used to retrieve the uploaded file
       picture = req.files.gambar;
-
+    
       const filename = `A${Date.now()}-${picture.name}`
       uploadPath = path.join(__dirname, '../public', 'images', filename);
 
-      console.log(uploadPath)
       // Use the mv() method to place the file somewhere on your server
 
       picture.mv(uploadPath, function (err) {
@@ -162,81 +162,91 @@ INNER JOIN barang bar ON bar.id_barang = var.id_barang WHERE barcode = $1;`
     }
   })
 
-  router.get('/edit/:id', async function (req, res) {
+//   router.get('/edit/:id', async function (req, res) {
+//     const { json } = req.headers
+
+//     try {
+//       let id = req.params.id
+//       let sql = `SELECT var.barcode,
+//                     var.varian_name,
+//                     bar.id_barang,
+//                     bar.nama_barang,
+//                       var.stok,
+//                       var.buy_price,
+//                       var.sell_price,
+//                       var.picture
+// FROM varian var
+// INNER JOIN barang bar ON bar.id_barang = var.id_barang WHERE barcode = $1;`
+//       const { rows } = await pool.query(sql, [id])
+//       if (json == 'true') {
+//         res.status(200).json(rows)
+//       } else {
+//         res.render('varian')
+//       }
+//     } catch (e) {
+//       console.log(e)
+//       res.status(500).json({ message: 'error get edit' })
+//     }
+
+//   });
+
+
+  router.put('/:id', async function (req, res) {
     const { json } = req.headers
 
+ 
     try {
-      let id = req.params.id
-      let sql = `SELECT var.barcode,
-                    var.varian_name,
-                    bar.id_barang,
-                    bar.nama_barang,
-                      var.stok,
-                      var.buy_price,
-                      var.sell_price,
-                      var.picture
-FROM varian var
-INNER JOIN barang bar ON bar.id_barang = var.id_barang WHERE barcode = $1;`
-      const { rows } = await pool.query(sql, [id])
-      if (json == 'true') {
-        res.status(200).json(rows)
-      } else {
-        res.render('varian')
-      }
-    } catch (e) {
-      console.log(e)
-      res.status(500).json({ message: 'error get edit' })
-    }
+      const { barcodeedit, nama_varianedit, stokedit, buy_priceedit, sell_priceedit, id_barangedit } = req.body
 
-  });
+      let picture;
+      let uploadPath;
 
 
-  router.put('/edit/:id', async function (req, res) {
-    const { json } = req.headers
-
-    const { custom_input, nama, stok, saved_pictures, harga_jual, harga_beli, barang } = req.body
-    let pictures;
-    let uploadPath;
-    if (!req.files || Object.keys(req.files).length === 0) {
-      db.query(`UPDATE varian set 
+      if (!req.files || Object.keys(req.files).length === 0) {
+        const edit = pool.query(`UPDATE varian set 
                 varian_name = $1,
-                stock = $2,
-                 pictures = $3,
-                  sell_price = $4,
-                   buy_price = $5,
-                  id_barang = $6,
-              WHERE barcode = $7`, [nama, stok, saved_pictures, harga_jual, harga_beli, barang, custom_input], (err) => {
-        if (err) {
-          return console.error(err.message);
+                stok = $2,
+                  sell_price = $3,
+                   buy_price = $4,
+                  id_barang = $5
+              WHERE barcode = $6`, [nama_varianedit, stokedit, buy_priceedit, sell_priceedit, id_barangedit, barcodeedit])
+
+        if (json == 'true') {
+          res.status(200).json(edit)
+        } else {
+          res.render('varian')
         }
-        res.redirect('/varian')
-      })
-    } else {
-      // The name of the input field (i.e. "pictures") is used to retrieve the uploaded file
-      pictures = req.files.pictures;
-      const filename = `A${Date.now()}-${pictures.name}`
-      uploadPath = path.join(__dirname, '/../public', 'pictures', filename);
-      // Use the mv() method to place the file somewhere on your server
-      pictures.mv(uploadPath, function (err) {
-        if (err)
-          return res.status(500).send(err);
-        //  const {custom_input, nama, barang, stok, harga, satuan, gudang } = req.body
 
-        db.query(`UPDATE varian set 
+      } else {
+        // The name of the input field (i.e. "pictures") is used to retrieve the uploaded file
+        picture = req.files.gambaredit;
+        const filename = `A${Date.now()}-${picture.name}`
+        uploadPath = path.join(__dirname, '../public', 'images', filename);
+        // Use the mv() method to place the file somewhere on your server
+        picture.mv(uploadPath, function (err) {
+          const { barcodeedit, nama_varianedit, stokedit, buy_priceedit, sell_priceedit, id_barangedit } = req.body
+          const filename1 = `{${filename}}`
+          console.log('fileupload', req.body)
+          const editfile = pool.query(`UPDATE varian set 
                 varian_name = $1,
-                stock = $2,
-                 pictures = $3,
+                stok = $2,
+                 picture = $3,
                   sell_price = $4,
                    buy_price = $5,
-                  id_barang = $6,
-              WHERE barcode = $7`, [nama, stok, saved_pictures, harga_jual, harga_beli, barang, custom_input], (err) => {
-          if (err) {
-            return console.error(err.message);
+                  id_barang = $6
+              WHERE barcode = $7`, [nama_varianedit, stokedit, filename1, buy_priceedit, sell_priceedit, id_barangedit, barcodeedit])
+
+          if (json == 'true') {
+            res.status(200).json(editfile)
+          } else {
+            res.render('varian')
           }
-          res.redirect('/varian')
         })
-      })
+      }
+    } catch (error) {
+      res.status(500).json({message: 'upload gagal'})
     }
+
   })
   router.get('/delete/:barcode', async function (req, res) {
     const { json } = req.headers
